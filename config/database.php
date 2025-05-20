f064a877693d32b4a133f8f50dbc0954656e6f00
rubio-dev <rubiovp12@gmail.com>
2025-05-20 15:44:50 -0700

    Cambios menores

---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private $host = '127.0.0.1'; // o mantener 'localhost'
+    private $port = '3307'; // PUERTO que muestra XAMPP
+    private $db_name = 'swift_invoice_db';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};port={$this->port};dbname={$this->db_name}", 
+                $this->username, 
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Connection error: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
